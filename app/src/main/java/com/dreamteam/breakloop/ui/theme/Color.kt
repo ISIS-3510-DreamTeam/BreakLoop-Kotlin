2a628242fe440c3a7b723cd41216f99e151b5900
sarixxxxx <s.gonzalezd@uniandes.edu.co>
2026-09-24 18:49:41 -0500

    feat: agregar algunos colores

--- app/src/main/java/com/dreamteam/breakloop/ui/theme/Color.kt
+++ app/src/main/java/com/dreamteam/breakloop/ui/theme/Color.kt
@@ -12,5 +12,25 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 object ColorPalette{
-    object
+    object SpicyPaprika {
+        val t100: Color= Color(0xFFF7E0D9)
+        val t500: Color= Color(0xFFC85A32)
+        val t700: Color= Color(0xFFA54E2E)
+    }
+    object GoldenOrange {
+        val t100: Color= Color(0xFFFBEBDA)
+        val t700: Color= Color(0xFFE59F3D)
+    }
+    object Fern {
+        val t100: Color= Color(0xFFE5E3DB)
+        val t300: Color= Color(0xFFA1AA93)
+        val t500: Color= Color(0xFF546A42)
+    }
+    object Neutral {
+        val Snow: Color= Color(0xFFFFF8F6)
+        val t100: Color= Color(0xFFEBE3E1)
+        val t200: Color= Color(0xFFCEC5C1)
+        val t500: Color= Color(0xFF9D918C)
+
+    }
 }
--- app/src/main/java/com/dreamteam/breakloop/ui/theme/Theme.kt
+++ app/src/main/java/com/dreamteam/breakloop/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.dreamteam.breakloop.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,9 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = ColorPalette.Neutral.t500,
+    secondary = ColorPalette.Fern.t500,
+    tertiary = ColorPalette.GoldenOrange.t700
 )
 
 private val LightColorScheme = lightColorScheme(
@@ -22,15 +21,6 @@ private val LightColorScheme = lightColorScheme(
     secondary = PurpleGrey40,
     tertiary = Pink40
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
