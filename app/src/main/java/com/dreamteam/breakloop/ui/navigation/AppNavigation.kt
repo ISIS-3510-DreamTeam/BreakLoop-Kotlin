890b317e57e2c48fc4f2f6d6fe50105e50d0fdf2
sarixxxxx <s.gonzalezd@uniandes.edu.co>
2026-09-24 22:35:44 -0500

    fix: bug pendejo (se me fue un corchete)

--- app/src/main/java/com/dreamteam/breakloop/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/dreamteam/breakloop/ui/navigation/AppNavigation.kt
@@ -1,43 +1,44 @@
 package com.dreamteam.breakloop.ui.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.dreamteam.breakloop.ui.account.navigation.AuthRoutes
 import com.dreamteam.breakloop.ui.account.navigation.authGraph
 
-
 object Routes {
-    const val AUTH_GRAPH= "auth_graph"
-    const val MAIN_GRAPH= "main_graph"
+    const val AUTH_GRAPH = "auth_graph"
+    const val MAIN_GRAPH = "main_graph"
 
-    const val HOME= "home"
+    const val HOME = "home"
     const val PROFILE = "profile"
     const val FOCUS = "focus"
-
-
 }
+
 fun NavHostController.safeNavigate(route: String, baseRoute: String) {
-    if( currentBackStackEntry?.destination?.route != route) {
+    if (currentBackStackEntry?.destination?.route != route) {
         navigate(route) {
-            popUpTo(baseRoute) { inclusive = false}
+            popUpTo(baseRoute) { inclusive = false }
             launchSingleTop = true
+        }
     }
 }
 
 @Composable
-fun AppNavigation(navController: NavHostController,
-                  isLoggedIn: Boolean) {
+fun AppNavigation(
+    navController: NavHostController,
+    isLoggedIn: Boolean
+) {
     NavHost(
         navController = navController,
         startDestination = if (isLoggedIn) Routes.MAIN_GRAPH else Routes.AUTH_GRAPH
     ) {
-        navigation (
+        navigation(
             route = Routes.AUTH_GRAPH,
             startDestination = AuthRoutes.SPLASH
-        ){
+        ) {
             authGraph(navController)
         }
         navigation(
@@ -53,9 +54,6 @@ fun AppNavigation(navController: NavHostController,
             composable(Routes.FOCUS) {
                 // TODO: Agregar FOCUS
             }
-            }
         }
-
     }
-
 }
