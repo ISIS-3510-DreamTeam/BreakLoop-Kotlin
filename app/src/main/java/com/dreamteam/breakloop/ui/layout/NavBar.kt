a1ea82d389ddd88a20b774e6e97dbcdc24559aeb
sarixxxxx <s.gonzalezd@uniandes.edu.co>
2026-09-24 23:03:38 -0500

    feat: cositas de navegación

---
+++ app/src/main/java/com/dreamteam/breakloop/ui/layout/NavBar.kt
@@ -0,0 +1,42 @@
+package com.dreamteam.breakloop.ui.layout
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import androidx.xr.compose.material3.NavigationSuiteScaffold
+
+@Composable
+fun NavBar(
+    navController: NavController
+) {
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            AppDestinations.entries.forEach {
+                item(
+                    icon = {
+                        Icon(
+                            painterResource(it.icon),
+                            contentDescription = it.label
+                        )
+                    },
+                    label = { Text(it.label) },
+                    selected = it == currentDestination,
+                    onClick = { currentDestination = it }
+                )
+            }
+        }
+    ) {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
+}
