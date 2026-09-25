67ccf0e658dd4e663743a7f690ed901594c723c9
sarixxxxx <s.gonzalezd@uniandes.edu.co>
2026-09-24 22:19:46 -0500

    feat: agregar base para grafo de navegación

---
+++ app/src/main/java/com/dreamteam/breakloop/ui/navigation/AppNavigation.kt
@@ -0,0 +1,61 @@
+package com.dreamteam.breakloop.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.navigation
+import com.dreamteam.breakloop.ui.account.navigation.AuthRoutes
+import com.dreamteam.breakloop.ui.account.navigation.authGraph
+
+
+object Routes {
+    const val AUTH_GRAPH= "auth_graph"
+    const val MAIN_GRAPH= "main_graph"
+
+    const val HOME= "home"
+    const val PROFILE = "profile"
+    const val FOCUS = "focus"
+
+
+}
+fun NavHostController.safeNavigate(route: String, baseRoute: String) {
+    if( currentBackStackEntry?.destination?.route != route) {
+        navigate(route) {
+            popUpTo(baseRoute) { inclusive = false}
+            launchSingleTop = true
+    }
+}
+
+@Composable
+fun AppNavigation(navController: NavHostController,
+                  isLoggedIn: Boolean) {
+    NavHost(
+        navController = navController,
+        startDestination = if (isLoggedIn) Routes.MAIN_GRAPH else Routes.AUTH_GRAPH
+    ) {
+        navigation (
+            route = Routes.AUTH_GRAPH,
+            startDestination = AuthRoutes.SPLASH
+        ){
+            authGraph(navController)
+        }
+        navigation(
+            route = Routes.MAIN_GRAPH,
+            startDestination = Routes.HOME
+        ) {
+            composable(Routes.HOME) {
+                // TODO: Agregar main
+            }
+            composable(Routes.PROFILE) {
+                // TODO: Agregar perfil
+            }
+            composable(Routes.FOCUS) {
+                // TODO: Agregar FOCUS
+            }
+            }
+        }
+
+    }
+
+}
